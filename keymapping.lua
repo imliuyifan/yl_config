400e3943d9cba734adf1efe99c6c43e19c84b3b2
Yifan Liu <imliuyifan@gmail.com>
2022-10-28 17:22:13 -0400

    telescope - default to vertical previewer

--- keymapping.lua
+++ keymapping.lua
@@ -5,7 +5,7 @@ lvim.keys.command_mode["jk"] = "<esc>"
 -- map a group of keys
 lvim.builtin.which_key.mappings["z"] ={
   name = "YL find",
-  c = {"<cmd>Telescope find_files cwd=~/.config<cr>", "Search config"},
-  d = {"<cmd>Telescope find_files cwd=~/dev<cr>", "Search dev"},
-  b = {"<cmd>Telescope current_buffer_fuzzy_find sorting_strategy=ascending layout_config={prompt_position='top'}<cr>", "Search buffer"},
+  c = {"<cmd>Telescope fd cwd=~/.config<cr>", "Search config"},
+  d = {"<cmd>Telescope fd cwd=~/dev<cr>", "Search dev"},
+  b = {"<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search buffer"},
 }
--- yl-telescope.lua
+++ yl-telescope.lua
@@ -2,6 +2,12 @@
 local _, action_state = pcall(require, "telescope.actions.state")
 lvim.builtin.telescope.defaults = {
   prompt_prefix = '> ',
+  layout_strategy = 'vertical',
+  sorting_strategy='ascending',
+  layout_config = {
+    mirror = true,
+    prompt_position='top',
+  },
   mappings = {
     i = {
       ["<c-a>"] = function() print(vim.inspect(action_state.get_selected_entry())) end
