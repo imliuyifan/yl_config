47e8e150bdfdaeb1f6198b0ff4aad42ce8548366
Yifan Liu <imliuyifan@gmail.com>
2022-10-28 16:33:48 -0400

    experiment with telescope config

--- keymapping.lua
+++ keymapping.lua
@@ -5,5 +5,7 @@ lvim.keys.command_mode["jk"] = "<esc>"
 -- map a group of keys
 lvim.builtin.which_key.mappings["z"] ={
   name = "YL find",
-  c = {"<cmd>Telescope find_files cwd=~/.config<cr>", "Search Config"},
+  c = {"<cmd>Telescope find_files cwd=~/.config<cr>", "Search config"},
+  d = {"<cmd>Telescope find_files cwd=~/dev<cr>", "Search dev"},
+  b = {"<cmd>Telescope current_buffer_fuzzy_find sorting_strategy=ascending layout_config={prompt_position='top'}<cr>", "Search buffer"},
 }
--- yl-telescope.lua
+++ yl-telescope.lua
@@ -1,4 +1,10 @@
 -- config telescope defaults
+local _, action_state = pcall(require, "telescope.actions.state")
 lvim.builtin.telescope.defaults = {
-  prompt_prefix = '> '
+  prompt_prefix = '> ',
+  mappings = {
+    i = {
+      ["<c-a>"] = function() print(vim.inspect(action_state.get_selected_entry())) end
+    }
+  }
 }
