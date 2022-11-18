49cf1a6017bfc0fd1a715fd20d3a2b1336f3d47a
Yifan Liu <imliuyifan@gmail.com>
2022-11-18 16:54:18 -0500

    experiment with telescope

--- keymapping.lua
+++ keymapping.lua
@@ -5,7 +5,11 @@ lvim.keys.command_mode["jk"] = "<esc>"
 -- map a group of keys
 lvim.builtin.which_key.mappings["z"] ={
   name = "YL find",
+  g = {"<cmd>Telescope git_files<cr>", "Git files"},
+  t = {"<cmd>Telescope git_commits<cr>", "Git commits"},
   c = {"<cmd>Telescope fd cwd=~/.config<cr>", "Search config"},
   d = {"<cmd>Telescope fd cwd=~/dev<cr>", "Search dev"},
   b = {"<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search buffer"},
+  l = {"<cmd>Telescope live_grep<cr>", "Live grep"},
+  s = {":Telescope grep_string search=", "Static grep"},
 }
